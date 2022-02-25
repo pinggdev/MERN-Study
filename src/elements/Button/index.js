95ed3ecbe50be4a9a1a1e9254615020829009e60
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2022-02-26 01:57:51 +0700

    Create Hero component

--- src/elements/Button/index.js
+++ src/elements/Button/index.js
@@ -9,7 +9,7 @@ export default function Button(props) {
   if (props.isLarge) className.push("btn-lg");
   if (props.isSmall) className.push("btn-sm");
   if (props.isBlock) className.push("btn-block");
-  if (props.isShadow) className.push("btn-shadow");
+  if (props.hasShadow) className.push("btn-shadow");
 
   const onClick = () => {
     if (props.onClick) props.onClick();
@@ -75,6 +75,7 @@ Button.propTypes = {
   href: propTypes.string,
   target: propTypes.string,
   className: propTypes.string,
+  isPrimary: propTypes.bool,
   isDisabled: propTypes.bool,
   isLoading: propTypes.bool,
   isSmall: propTypes.bool,
