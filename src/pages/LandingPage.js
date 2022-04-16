cd45b7bde6686a2805234c7bb9e8bbf97e9407ad
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2022-04-16 19:06:56 +0700

    adding component testimony

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -23,7 +23,7 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
-        <Testimony />
+        <Testimony data={landingPage.testimonial} />
       </>
     );
   }
