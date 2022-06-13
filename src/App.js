882c07c68179cebad200460b217de789d6f6cd89
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2022-06-13 23:14:09 +0700

    parts page detail title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import "./assets/scss/style.scss";
-import LandingPage from "pages/LandingPage";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import LandingPage from "pages/LandingPage";
+import DetailsPage from "pages/DetailsPage";
 import Example from "pages/Example";
+
+import "./assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={LandingPage} />
-      </Router>
-      <Router>
+        <Route exact path="/properties/:id" component={DetailsPage} />
         <Route exact path="/example" component={Example} />
       </Router>
     </div>
