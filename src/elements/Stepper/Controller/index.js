a653d4b24d987f882badf6a8a60c28cd081c3f9d
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2022-06-20 20:57:24 +0700

    adding MainContent stepper element

--- src/elements/Stepper/Controller/index.js
+++ src/elements/Stepper/Controller/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
 
-export default function index(props) {
+export default function Controller(props) {
   return (
     <Fade>
       <section className="container">
